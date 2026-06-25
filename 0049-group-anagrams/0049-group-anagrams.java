9970a31162884b09c3ce2afba29a0318f18381db
VUPPERPALLY BHAVITHA <vupperpallybhavitha@gmail.com>
2026-06-25 21:23:42 +0530

    Time: 6 ms (99.42%), Space: 49.6 MB (72.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> li=new ArrayList<>();
-        Map<String,List<String>> mp=new HashMap<>();
+        Map<String,ArrayList<String>> mp=new HashMap<>();
         for(String s:strs)
         {
             char[] ch=s.toCharArray();
@@ -12,5 +12,5 @@ class Solution {
             mp.get(sorted).add(s);
         }
        return new ArrayList<>(mp.values());
-    }
+    };
 }
