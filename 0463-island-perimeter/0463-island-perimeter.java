21714bc04f1a2e01765acd0a525e590f7e6c4442
VUPPERPALLY BHAVITHA <vupperpallybhavitha@gmail.com>
2026-07-09 20:34:14 +0530

    Time: 4 ms (99.89%), Space: 47.3 MB (38.25%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,25 +1,23 @@
-public class Solution {
+class Solution {
     public int islandPerimeter(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    if ((j > 0 && grid[i][j - 1] == 0) || j == 0)
-                        cnt++;
-
-                    if ((i > 0 && grid[i - 1][j] == 0) || i == 0)
-                        cnt++;
-
-                    if ((j < m - 1 && grid[i][j + 1] == 0) || j == m - 1)
-                        cnt++;
-
-                    if ((i < n - 1 && grid[i + 1][j] == 0) || i == n - 1)
-                        cnt++;
+       int peri=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    if(i==0||grid[i-1][j]==0)
+                    peri++;
+                    if(i==grid.length-1||grid[i+1][j]==0)
+                    peri++;
+                    if(j==0||grid[i][j-1]==0)
+                    peri++;
+                    if(j==grid[0].length-1||grid[i][j+1]==0)
+                    peri++;
                 }
             }
         }
-        return cnt;
+        return peri;
     }
 }
