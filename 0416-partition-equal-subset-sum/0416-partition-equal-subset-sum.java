78df6b6339ac07c1f60f520f240576ee43ab8fa5
VUPPERPALLY BHAVITHA <vupperpallybhavitha@gmail.com>
2026-08-21 14:24:12 +0530

    Time: 96 ms (20.36%), Space: 75.2 MB (21.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,25 +1,34 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int totSum = 0;
+     
         
-        int sum=0;
-        for(int ele:nums)
-        {
-            sum+=ele;
+        for(int i = 0; i<nums.length; i++){
+            totSum+=nums[i];
         }
-        if(sum%2!=0)
+        if(totSum%2!=0) return false;
+        int target = totSum/2;
+           Boolean[][] dp = new Boolean[nums.length+1][target+1]; 
+           
+       return function(nums.length-1,target, nums,dp);
+    }
+    public boolean function(int i, int target, int[] nums, Boolean[][] dp){
+        if(target == 0){
+            return true;
+        }
+        
+      if(i<0){
         return false;
-        int target=sum/2;
-        boolean[] dp=new boolean[target+1];
-        dp[0]=true;
-        for(int num:nums)
-        {
-            for(int i=target;i>=num;i--)
-            {
-                dp[i]=dp[i]||dp[i-num];
+      }
+      if(dp[i][target] != null){
+      return   dp[i][target];
+      }
+       boolean notTake = function(i-1, target,nums,dp);
+       boolean take = false;
+       if(nums[i]<=target){
+         take =  function (i-1,target-nums[i],nums,dp);
+       }
 
-                if(dp[target]) return true;
-            }
-        }
-        return dp[target];
+       return  dp[i][target] = take || notTake;
     }
 }
