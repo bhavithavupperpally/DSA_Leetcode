24ff3064ab79905b7cf14e4cb0a1de72a3b4eada
VUPPERPALLY BHAVITHA <vupperpallybhavitha@gmail.com>
2026-09-12 21:18:33 +0530

    Time: 5 ms (9.57%), Space: 47.8 MB (56.83%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,55 +1,73 @@
 class Solution {
-    public void dfs(char[][] board,int row,int col)
+
+    int[] leader;
+
+    int find(int x)
     {
-        if(row<0||col<0||row>=board.length||col>=board[0].length)
-        return;
-        if(board[row][col]!='O')
-        return;
-        board[row][col]='#';
-        dfs(board,row+1,col);
-        dfs(board,row-1,col);
-        dfs(board,row,col+1);
-        dfs(board,row,col-1);
+        if(x == leader[x])
+            return x;
+
+        return leader[x] = find(leader[x]);
     }
+
+    void merge(int a, int b)
+    {
+        int sla = find(a);
+        int slb = find(b);
+
+        if(sla != slb)
+            leader[sla] = slb;
+    }
+
     public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int i=0;i<board[0].length;i++)
+
+        int m = board.length;
+        int n = board[0].length;
+        int dummy=m*n;
+        leader=new int[m*n+1];
+        for(int i = 0; i < leader.length; i++)
         {
-            if(board[0][i]=='O')
-            {
-                dfs(board,0,i);
-            }
-        }
-        for(int i=0;i<board[0].length;i++)
-        {
-                 if(board[m-1][i]=='O')
-            {
-                dfs(board,m-1,i);
-            }
-        }
-        for(int i=0;i<board.length;i++)
-        {
-            if(board[i][0]=='O')
-            {
-                dfs(board,i,0);
-            }
-        }
-        for(int i=0;i<board.length;i++)
-        {
-             if(board[i][n-1]=='O')
-            {
-                dfs(board,i,n-1);
-            }
+            leader[i] = i;
         }
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(board[i][j]=='O')
-                board[i][j]='X';
-                else if(board[i][j]=='#')
-                board[i][j]='O';
+                if(board[i][j]!='O')
+                continue;
+                int current=i*n+j;
+
+                if(i==0||i==m-1||j==0||j==n-1)
+                {
+                    merge(current,dummy);
+                }
+                if(i+1<m&&board[i+1][j]=='O')
+                merge(current,(i+1)*n+j);
+                if(i-1>=0&&board[i-1][j]=='O')
+                merge(current,(i-1)*n+j);
+                if(j+1<n&&board[i][j+1]=='O')
+                merge(current,(i)*n+j+1);
+                if(j-1>=0&&board[i][j-1]=='O')
+                merge(current,(i)*n+j-1);
+
+            }
+        }
+        
+
+       
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(board[i][j] == 'O')
+                {
+                    int current = i * n + j;
+
+                    if(find(current) != find(dummy))
+                    {
+                        board[i][j] = 'X';
+                    }
+                }
             }
         }
     }
