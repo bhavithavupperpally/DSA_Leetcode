47004e26b1b832f720247fdd959234a87b9c0a75
VUPPERPALLY BHAVITHA <vupperpallybhavitha@gmail.com>
2026-09-12 17:47:06 +0530

    Time: 12 ms (98.25%), Space: 240.2 MB (99.03%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,38 +1,42 @@
 class Solution {
-
-            public boolean dfs(int node,int destination,ArrayList<ArrayList<Integer>> adj,HashSet<Integer> visited)
-            {
-
-                if(node==destination)
-                return true;
-                visited.add(node);
-                for(int neighbour:adj.get(node))
-                {
-                    if(!visited.contains(neighbour))
-                    
-                    if(dfs(neighbour,destination,adj,visited))
-                    return true;
-                }
-                return false;
-            }
+   int[] leader;
+   int size[];
+   int find(int x)
+   {
+    if(x==leader[x])
+    return x;
+    return leader[x]=find(leader[x]);
+   }
+   void merge(int a,int b)
+   {
+    int sla=find(a);
+    int slb=find(b);
+    if(sla!=slb)
+    {
+    
+    leader[sla]=slb;
+    }
+ }
    
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        HashSet<Integer> visited=new HashSet<>();
+       leader=new int[n];
+       size=new int[n];
         for(int i=0;i<n;i++)
         {
-            adj.add(new ArrayList<>());
-            
+            leader[i]=i;
+            size[i]=1;
         }
-        
         for(int i=0;i<edges.length;i++)
         {
             int u=edges[i][0];
             int v=edges[i][1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            merge(u,v);
         }
-            boolean ans=dfs(source,destination,adj,visited);
-            return ans;
+        if(find(source)==find(destination))
+        return true;
+    else
+        return false;
+       
+
     }
 }
